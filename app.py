5853702f2e18265df1962a3b0e77b5b4e9e9f78c
Gazi Mubtasim Tousif Nuhan <mubtasimtousif@gmail.com>
2026-04-10 12:57:59 +0600

    updated

--- app.py
+++ app.py
@@ -187,4 +187,4 @@ st.download_button("Download CSV", csv, "clustered_output.csv", "text/csv")
 
 # footer
 st.markdown("---")
-st.caption("Original Template by @Rashedul Alam | Enhanced & Maintained by @Tousif Nuhan")
+st.caption("Developed by Tousif Nuhan | Based on an open-source template by Rashedul Alam")
